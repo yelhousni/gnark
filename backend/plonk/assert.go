847a74680a7f9f236da7218b280017f810285b03
Youssef El Housni <youssef.housni21@gmail.com>
2021-08-03 22:54:19 +0100

    fix(bw6-633): add bw6-633 to plonk assert

--- backend/plonk/assert.go
+++ backend/plonk/assert.go
@@ -26,12 +26,14 @@ import (
 	cs_bls12381 "github.com/consensys/gnark/internal/backend/bls12-381/cs"
 	cs_bls24315 "github.com/consensys/gnark/internal/backend/bls24-315/cs"
 	cs_bn254 "github.com/consensys/gnark/internal/backend/bn254/cs"
+	cs_bw6633 "github.com/consensys/gnark/internal/backend/bw6-633/cs"
 	cs_bw6761 "github.com/consensys/gnark/internal/backend/bw6-761/cs"
 
 	witness_bls12377 "github.com/consensys/gnark/internal/backend/bls12-377/witness"
 	witness_bls12381 "github.com/consensys/gnark/internal/backend/bls12-381/witness"
 	witness_bls24315 "github.com/consensys/gnark/internal/backend/bls24-315/witness"
 	witness_bn254 "github.com/consensys/gnark/internal/backend/bn254/witness"
+	witness_bw6633 "github.com/consensys/gnark/internal/backend/bw6-633/witness"
 	witness_bw6761 "github.com/consensys/gnark/internal/backend/bw6-761/witness"
 
 	"github.com/consensys/gnark-crypto/ecc"
@@ -39,6 +41,7 @@ import (
 	kzg_bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381/fr/kzg"
 	kzg_bls24315 "github.com/consensys/gnark-crypto/ecc/bls24-315/fr/kzg"
 	kzg_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/kzg"
+	kzg_bw6633 "github.com/consensys/gnark-crypto/ecc/bw6-633/fr/kzg"
 	kzg_bw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761/fr/kzg"
 	"github.com/consensys/gnark-crypto/kzg"
 )
@@ -163,6 +166,9 @@ func newKZGSrs(ccs frontend.CompiledConstraintSystem) (kzg.SRS, error) {
 	case *cs_bls24315.SparseR1CS:
 		size := uint64(len(tccs.Constraints) + len(tccs.Assertions) + tccs.NbPublicVariables)
 		return kzg_bls24315.NewSRS(ecc.NextPowerOfTwo(size)+3, fakeRandomness)
+	case *cs_bw6633.SparseR1CS:
+		size := uint64(len(tccs.Constraints) + len(tccs.Assertions) + tccs.NbPublicVariables)
+		return kzg_bw6633.NewSRS(ecc.NextPowerOfTwo(size)+3, fakeRandomness)
 	default:
 		panic("unknown constraint system type")
 	}
